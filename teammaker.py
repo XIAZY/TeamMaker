22fd0c864e6dfee9645bab65490305159b06c9b7
XIAZY <admin@niconiconi.rocks>
2016-03-08 15:03:07 +0800

    add file dialog

--- teammaker.py
+++ teammaker.py
@@ -1,34 +1,35 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from ui import Ui_MainWindow
 import sys
 import os
 import random
-import time
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MyMainWindow, self).__init__()
         self.setupUi(self)
-        # self.label.setText('Pros')
-        # self.label_2.setText('Cons')
         self.pushButton.clicked.connect(self.do)
+        self.pushButton_2.clicked.connect(self.get_file_path)
+
+    def get_file_path(self):
+        self.file_path=QFileDialog.getOpenFileName(self,'Open File',os.getcwd())[0]
 
     def do(self):
         self.listWidget.clear()
         self.listWidget_2.clear()
-        path = os.path.join(os.getcwd(), 'namelist.txt')
+        if hasattr(self,'file_path') is False:
+            self.file_path = os.path.join(os.getcwd(), 'namelist.txt')
         try:
             people_num = self.spinBox.value()
-            with open(path, 'r') as file:
+            with open(self.file_path, 'r') as file:
                 namelist = file.readlines()
             if people_num * 2 > len(namelist):
                 raise ValueError
             random.shuffle(namelist)
-            namelist=namelist[:people_num*2]
+            namelist = namelist[:people_num * 2]
             for i in range(len(namelist)):
-                namelist[i]=namelist[i].replace('\n','')
+                namelist[i] = namelist[i].replace('\n', '')
             self.listWidget.addItems(namelist[:people_num])
             self.listWidget_2.addItems(namelist[people_num:])
         except FileNotFoundError as e:
